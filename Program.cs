2ae3f65d01dc965933e7394db4c18bea95dcf6dd
Taylor Southwick <tasou@microsoft.com>
2017-08-30 10:09:18 -0700

    Add parsing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using NuGet.Frameworks;
+
+using static NuGet.Frameworks.FrameworkConstants.CommonFrameworks;
+using System.Runtime.Versioning;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace NuGetPackageInfo
 {
@@ -6,7 +12,38 @@ namespace NuGetPackageInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: TargetFramework NuGetFramework1 [NuGetFramework2 ...]");
+                return;
+            }
+
+            Run(new FrameworkName(args[0]), args.Skip(1));
+        }
+
+        private static void Run(FrameworkName frameworkName, IEnumerable<string> nugetProfiles)
+        {
+            var target = NuGetFramework.ParseFrameworkName(frameworkName.FullName, DefaultFrameworkNameProvider.Instance);
+            var available = nugetProfiles.Select(NuGetFramework.ParseFolder).ToArray();
+
+            Console.WriteLine($"From: {target}");
+            Console.WriteLine("Available:");
+            foreach (var profile in available)
+            {
+                Console.WriteLine($"\t{profile}");
+            }
+
+            var reducer = new FrameworkReducer();
+            var result = reducer.GetNearest(target, available);
+
+            if (result == null)
+            {
+                Console.WriteLine("No available framework");
+            }
+            else
+            {
+                Console.WriteLine($"Best framework: {result}");
+            }
         }
     }
 }
