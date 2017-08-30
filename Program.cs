4b8c7d38b9952b27c53479ce2089b471444c221a
Taylor Southwick <tasou@microsoft.com>
2017-08-30 09:38:14 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NuGetPackageInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
